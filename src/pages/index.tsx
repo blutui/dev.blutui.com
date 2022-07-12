8853645e5281fd0d459f883b0ca18334da53b230
Jayan Ratna <jayan@blutui.com>
2022-07-13 07:35:44 +1200

    add additional meta tags to head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,10 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <title>Blutui</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="referrer" content="strict-origin" />
         <meta name="description" content="Blutui Developer Documentation" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
