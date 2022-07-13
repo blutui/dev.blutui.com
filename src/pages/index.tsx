fbfa5dfd4eae7e19161c939fd16062c686401731
Jayan Ratna <jayan@blutui.com>
2022-07-13 15:10:31 +1200

    Update index page styles to tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,26 @@
 import { NextPage } from 'next'
-import Head from 'next/head'
 
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+    <div className="px-8">
+      <main className="min-h-screen py-16 flex flex-1 flex-col justify-center items-center">
+        <h1 className="text-6xl font-bold text-center">
+          Welcome to{' '}
+          <a
+            href="https://nextjs.org"
+            className="text-indigo-500 hover:underline"
+          >
+            Next.js!
+          </a>
         </h1>
 
-        <p className={styles.description}>
+        <p className="my-12 text-2xl text-center">
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          <code className="bg-gray-100 px-3 py-2 text-base font-mono rounded-md">
+            pages/index.tsx
+          </code>
         </p>
       </main>
     </div>
