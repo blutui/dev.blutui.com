849395e2cdd77e63c73acc486397458725d000ac
Jayan Ratna <jayan@blutui.com>
2022-09-16 09:39:36 +1200

    add sticky sidebar elements

--- src/layouts/DocumentationLayout.tsx
+++ src/layouts/DocumentationLayout.tsx
@@ -14,10 +14,10 @@ export const DocumentationLayout = ({
   return (
     <>
       <main className="flex-1 my-8 px-8 mx-auto w-full max-w-8xl flex items-start space-x-6">
-        <div className="flex-shrink-0 border border-black/10 dark:border-white/10 rounded-lg w-72 py-6"></div>
+        <div className="flex-shrink-0 sticky top-[5.8125rem] border border-black/10 dark:border-white/10 rounded-lg w-72 py-6"></div>
         <div className="flex-auto flex items-start space-x-6">
           <div className="flex-auto">{children}</div>
-          <div className="flex-shrink-0 w-64">
+          <div className="flex-shrink-0 sticky top-[5.8125rem] w-64">
             <Contribution />
           </div>
         </div>
