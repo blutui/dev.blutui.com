36d5b3bda53ab3a8e229c69d748a300fef9df6e3
Jayan Ratna <jayan@blutui.com>
2022-09-16 09:06:17 +1200

    add layouts directory to content array

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./src/pages/**/*.{ts,tsx}",
+    "./src/layouts/**/*.{ts,tsx}",
     "./src/components/**/*.{ts,tsx}"
   ],
   theme: {
