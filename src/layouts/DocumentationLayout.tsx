53b6da3d1d6e5ed32ef34e94e165b7dac81c3251
Jayan Ratna <jayan@blutui.com>
2022-09-16 13:50:17 +1200

    remove header border

--- src/layouts/DocumentationLayout.tsx
+++ src/layouts/DocumentationLayout.tsx
@@ -19,10 +19,10 @@ export const DocumentationLayout = ({
   return (
     <>
       <main className="flex-1 my-8 px-8 mx-auto w-full max-w-8xl flex items-start space-x-8">
-        <div className="flex-shrink-0 sticky top-[5.8125rem] border border-black/10 dark:border-white/10 rounded-lg w-72 py-6"></div>
+        <div className="flex-shrink-0 sticky top-[5.75rem] border border-black/10 dark:border-white/10 rounded-lg w-72 py-6"></div>
         <div className="flex-auto flex items-start space-x-8">
           <div className="flex-auto">{children}</div>
-          <div className="flex-shrink-0 sticky top-[5.8125rem] w-64 space-y-6">
+          <div className="flex-shrink-0 sticky top-[5.75rem] w-64 space-y-6">
             <TableOfContents toc={toc} />
             <Contribution />
           </div>
