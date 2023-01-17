832896acb8883c71acceefc5bba12de43ac9f2a0
Jayan Ratna <jayan@blutui.com>
2023-01-18 12:50:55 +1300

    use @next/font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html className="h-full antialiased" lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=optional"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head />
       <body className="relative h-full bg-zinc-50 dark:bg-zinc-900">
         <Main />
         <NextScript />
