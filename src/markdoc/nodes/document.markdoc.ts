e07bee8a72b140ca4827982be7ecf333c3a0fef1
Jayan Ratna <jayan@blutui.com>
2023-01-23 10:42:31 +1300

    use path alias to import files

--- src/markdoc/nodes/document.markdoc.ts
+++ src/markdoc/nodes/document.markdoc.ts
@@ -1,5 +1,5 @@
 import { Config, nodes, Schema } from '@markdoc/markdoc'
-import { Article } from '../../components/Article'
+import { Article } from '@/components/Article'
 
 export const document: Schema<Config, typeof Article> = {
   ...nodes.document,
