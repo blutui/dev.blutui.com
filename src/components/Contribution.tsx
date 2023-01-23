6007c100d890da5caf30f0bca8a8331f259f33b0
Jayan Ratna <jayan@blutui.com>
2023-01-24 12:45:56 +1300

    improve TOC component

--- src/components/Contribution.tsx
+++ src/components/Contribution.tsx
@@ -20,31 +20,21 @@ export const Contribution = () => {
     : 'https://github.com/blutui/dev.blutui.com'
 
   return (
-    <div className="max-w-xs rounded-lg border p-6 dark:border-zinc-700">
-      <h2 className="mb-4 font-semibold tracking-tight text-zinc-700 dark:text-zinc-200">
-        Help us make these docs great!
-      </h2>
-      <p className="mb-4 text-sm text-zinc-600 dark:text-zinc-400">
-        All Blutui docs are open source. See something that{"'"}s wrong or
-        unclear? Submit a pull request.
-      </p>
+    <div className="flex flex-col gap-2">
       <a
-        className="rounded-md border px-4 py-2 font-semibold text-indigo-600 transition hover:border-zinc-300 hover:bg-zinc-100 dark:border-zinc-700 dark:text-indigo-400 dark:hover:border-zinc-700 dark:hover:bg-zinc-100/10"
+        className="text-xs font-medium text-gray-500 transition hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+        href="https://github.com/blutui/dev.blutui.com/issues"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Question? Give us feedback →
+      </a>
+      <a
+        className="text-xs font-medium text-gray-500 transition hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
         href={contributionHref}
       >
-        Make a contribution
+        Edit this page on GitHub
       </a>
-      <p className="mt-3 text-xs text-zinc-600 dark:text-zinc-400">
-        or{' '}
-        <a
-          className="text-indigo-600 dark:text-indigo-400"
-          href="https://github.com/blutui/dev.blutui.com/blob/main/CONTRIBUTING.md"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          learn how to contribute
-        </a>
-      </p>
     </div>
   )
 }
