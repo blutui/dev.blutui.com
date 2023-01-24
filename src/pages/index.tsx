4c9f2b25293e273d43c7c5cc7b5c0e9e0d2beb06
Jayan Ratna <jayan@blutui.com>
2023-01-25 11:48:46 +1300

    add small footer to documentation layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,18 @@ import type { NextPageWithCustomLayout } from '@/types'
 
 const Home: NextPageWithCustomLayout = () => {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center py-16 text-center">
-      <h1 className="text-6xl font-bold tracking-tight text-gray-900 dark:text-gray-200">
-        Welcome to{' '}
-        <a
-          href="https://blutui.com"
-          className="text-indigo-500 hover:underline"
-        >
-          blutui.com
-        </a>
-      </h1>
+    <div className="-mx-8 -my-10 flex-grow px-8">
+      <div className="flex flex-1 flex-col items-center justify-center py-16 text-center">
+        <h1 className="text-6xl font-bold tracking-tight text-gray-900 dark:text-gray-200">
+          Welcome to{' '}
+          <a
+            href="https://blutui.com"
+            className="text-indigo-500 hover:underline"
+          >
+            blutui.com
+          </a>
+        </h1>
+      </div>
     </div>
   )
 }
