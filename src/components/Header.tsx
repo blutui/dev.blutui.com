fecea7a77dad7044786917ee573c1af254b9dbf8
Jayan Ratna <jayan@blutui.com>
2023-01-26 11:25:06 +1300

    wip: add homepage content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,8 +24,8 @@ export const Header = ({ children }: HeaderProps) => {
           <span className="-ml-px hidden h-9 border-l border-black/5 dark:border-white/5 lg:block"></span>
         </div>
         <div className="flex flex-auto items-center space-x-6">
-          <SearchButton className="hidden h-9 rounded-lg bg-zinc-900/10 px-3.5 py-1.5 text-left font-medium tracking-tight text-zinc-500 transition hover:bg-black/20 dark:bg-zinc-100/10 dark:text-zinc-400 dark:hover:bg-white/20 lg:block">
-            <div className="flex items-center gap-2 lg:w-80">
+          <SearchButton className="hidden h-9 w-full rounded-lg bg-zinc-900/10 px-3.5 py-1.5 text-left font-medium tracking-tight text-black/60 transition hover:bg-black/20 dark:bg-zinc-100/10 dark:text-white/60 dark:hover:bg-white/20 lg:block lg:max-w-sm">
+            <div className="flex items-center gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
@@ -40,7 +40,7 @@ export const Header = ({ children }: HeaderProps) => {
               </svg>
               <span className="block flex-auto">Quick search...</span>
               {actionKey && (
-                <kbd className="ml-2 -mr-1 rounded bg-white/10 py-0.5 px-1.5 font-sans text-xs font-semibold">
+                <kbd className="ml-2 -mr-1 rounded bg-black/10 py-0.5 px-1.5 font-sans text-xs font-semibold highlight-white/10 dark:bg-white/10">
                   <abbr title={actionKey[1]} className="no-underline">
                     {actionKey[0]}
                   </abbr>{' '}
@@ -97,24 +97,26 @@ export const Header = ({ children }: HeaderProps) => {
               </a>
             </li>
           </ul>
-          <span className="h-6 border-l border-black/10 dark:border-white/10"></span>
-          <a
-            href="https://github.com/blutui/dev.blutui.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <svg
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-              className="h-6 w-6 fill-zinc-400"
+          <div className="hidden items-center gap-6 sm:flex">
+            <span className="h-6 border-l border-black/10 dark:border-white/10"></span>
+            <a
+              href="https://github.com/blutui/dev.blutui.com"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"
-              />
-            </svg>
-          </a>
+              <svg
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+                className="h-6 w-6 fill-zinc-700 dark:fill-zinc-400"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"
+                />
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
     </header>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,33 @@
 import { FullPageLayout } from '@/layouts/FullPageLayout'
 
 import type { NextPageWithCustomLayout } from '@/types'
+import Link from 'next/link'
 
 const Home: NextPageWithCustomLayout = () => {
   return (
-    <div className="-mx-8 -my-10 flex-grow px-8">
-      <div className="flex flex-1 flex-col items-center justify-center py-16 text-center">
-        <h1 className="text-6xl font-bold tracking-tight text-gray-900 dark:text-gray-200">
-          Welcome to{' '}
-          <a
-            href="https://blutui.com"
-            className="text-indigo-500 hover:underline"
+    <div className="relative -mx-8 -mt-[6.25rem] flex-grow overflow-hidden rounded-xl px-8 pt-[6.25rem] pb-10">
+      <div className="absolute inset-0 -z-20 animate-pulse">
+        <div className="h-full w-full bg-gradient-to-b from-purple-400 via-orange-300 to-transparent"></div>
+      </div>
+      <div className="absolute top-0 left-0 -z-10 h-screen w-full bg-dots from-zinc-50 to-zinc-50 bg-[length:14px_14px] dark:from-zinc-900 dark:to-zinc-900"></div>
+      <div className="mx-auto max-w-screen-xl">
+        <div className="bg-zinc-50/90 py-10 px-8 text-left shadow-[0_0_128px_128px] shadow-zinc-50/90 dark:bg-zinc-900/90 dark:shadow-zinc-900/90">
+          <p className="mb-2 text-xs uppercase tracking-widest text-white/50">
+            Developer Documentation
+          </p>
+          <h1 className="text-4xl font-bold tracking-tight text-gray-800 dark:text-white">
+            Welcome to Blutui
+          </h1>
+          <p className="mt-2 mb-4 text-xl text-gray-800/80 dark:text-white/80">
+            Explore our guides and documentation on using Blutui.
+          </p>
+          <Link
+            href="/docs"
+            className="inline-flex h-9 select-none items-center whitespace-nowrap rounded-lg bg-indigo-500 px-4 text-sm font-semibold tracking-tight text-white transition highlight-white/10 hover:bg-indigo-400"
           >
-            blutui.com
-          </a>
-        </h1>
+            Get started
+          </Link>
+        </div>
       </div>
     </div>
   )
