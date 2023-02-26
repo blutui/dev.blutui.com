9bf3d346180f6264344da917a2608ff8cd605189
Jayan Ratna <jayan@blutui.com>
2023-02-27 09:06:53 +1300

    update to Next 13.2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 
 import { DocumentationLayout } from '@/layouts/DocumentationLayout'
 import { FullPageLayout } from '@/layouts/FullPageLayout'
