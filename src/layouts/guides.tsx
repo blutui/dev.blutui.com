529976ec2f7c2140d82cbd7364a4c529c4361c39
Jayan Ratna <jayan@blutui.com>
2023-08-28 14:15:25 +1200

    style single guide layout

--- src/layouts/guides.tsx
+++ src/layouts/guides.tsx
@@ -19,24 +19,26 @@ export const GuidesLayout = ({ layoutProps, children }: GuidesLayoutProps) => {
 
   return (
     <>
-      <main>
-        <header className="mb-6 border-b py-6 dark:border-zinc-700">
-          <div className="max-w-8xl mx-auto px-8">
-            <div className="mb-6">
+      <main className="">
+        <header className="mb-6 border-b px-8 py-8 dark:border-zinc-800">
+          <div className="mx-auto max-w-screen-md">
+            <div className="mb-4">
               <Link
                 href="/guides"
-                className="flex items-center gap-2 text-sm text-zinc-600 transition hover:text-zinc-400 dark:text-zinc-400 dark:hover:text-zinc-500"
+                className="inline-flex items-center gap-2 text-sm text-zinc-600 transition hover:text-zinc-400 dark:text-zinc-400 dark:hover:text-zinc-500"
               >
                 <ArrowLongLeft className="h-5 w-5" />
                 Back to guides
               </Link>
             </div>
-            <h1 className="text-2xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-200">
+            <h1 className="text-3xl font-bold tracking-tight text-zinc-900 dark:text-zinc-200 lg:text-4xl">
               {title}
             </h1>
           </div>
         </header>
-        {children}
+        <div className="px-8">
+          <div className="max-w-8xl mx-auto w-full">{children}</div>
+        </div>
       </main>
     </>
   )
