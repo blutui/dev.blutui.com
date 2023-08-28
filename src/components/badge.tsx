6e0fbb8630f0f41a8ea3f457f62d5965ce68113b
Jayan Ratna <jayan@blutui.com>
2023-08-28 15:25:25 +1200

    update badge styles

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -15,12 +15,15 @@ export interface BadgeProps {
 }
 
 const classes: Record<BadgeStatus, string> = {
-  default: 'bg-zinc-200 dark:bg-zinc-700 text-zinc-600 dark:text-zinc-300',
-  info: 'bg-blue-200 dark:bg-blue-900 text-blue-600 dark:text-blue-200',
-  success: 'bg-green-200 dark:bg-green-900 text-green-700 dark:text-green-200',
+  default:
+    'bg-zinc-200/20 dark:bg-zinc-700/20 ring-zinc-200 dark:ring-zinc-700 text-zinc-600 dark:text-zinc-300',
+  info: 'bg-blue-200/20 dark:bg-blue-900/20 ring-blue-200 dark:ring-blue-900 text-blue-600 dark:text-blue-200',
+  success:
+    'bg-green-200/20 dark:bg-green-900/20 ring-green-200 dark:ring-green-900 text-green-700 dark:text-green-200',
   attention:
-    'bg-yellow-200 dark:bg-yellow-900 text-yellow-700 dark:text-yellow-200',
-  warning: 'bg-red-200 dark:bg-red-900 text-red-700 dark:text-red-200',
+    'bg-yellow-200/20 dark:bg-yellow-900/20 ring-yellow-200 dark:ring-yellow-900 text-yellow-700 dark:text-yellow-200',
+  warning:
+    'bg-red-200/20 dark:bg-red-900/20 ring-red-200 dark:ring-red-900 text-red-600 dark:text-red-300',
   critical: '',
 }
 
@@ -28,7 +31,7 @@ export const Badge = ({ status, children }: BadgeProps) => {
   return (
     <span
       className={cn(
-        'inline-flex items-center rounded-full py-0.5 px-3 text-sm font-semibold tracking-tight',
+        'inline-flex items-center rounded-full px-3 py-0.5 text-sm font-semibold tracking-tight ring-1',
         classes[status]
       )}
     >
