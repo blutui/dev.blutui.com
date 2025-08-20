89903ef2f23a9423fc1b9c23ab8b2e200750253c
Jayan Ratna <jayan@blutui.com>
2025-08-21 09:38:01 +1200

    add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,20 @@
 import { MetadataRoute } from 'next'
+import { getAllPages } from 'utils/pages'
+import { getServerSideURL } from 'utils/url'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return []
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const docs = await getAllPages('docs')
+
+  const url: string = getServerSideURL()
+
+  return [
+    {
+      url,
+      priority: 1,
+    },
+    ...docs.map((path): MetadataRoute.Sitemap[number] => ({
+      url: `${url}/${path}`,
+      priority: 0.8,
+    })),
+  ]
 }
--- utils/pages.ts
+++ utils/pages.ts
@@ -3,23 +3,26 @@
 import { readdir } from 'node:fs/promises'
 import path from 'node:path'
 
+export const getAllPages = async (directory: string, base = ''): Promise<string[]> => {
+  const contentDir = path.join(process.cwd(), 'content')
+  const content = path.join(contentDir, directory.replace(contentDir, ''))
+
+  const entries = await readdir(content, { withFileTypes: true })
+  const files = await Promise.all(
+    entries.map(async (entry) => {
+      const res = path.join(content, entry.name)
+      const rel = path.join(base, entry.name)
+      if (entry.isDirectory()) return getAllPages(res, rel)
+      if (entry.isFile() && entry.name.endsWith('.md')) return [rel.replace(/\.md$/, '')]
+      return []
+    })
+  )
+  return files.flat()
+}
+
 export function generateStaticParamsFor(contentDir: string, segmentKey: string = 'slug') {
   return async () => {
-    const getAllMd = async (dir: string, base = ''): Promise<string[]> => {
-      const entries = await readdir(dir, { withFileTypes: true })
-      const files = await Promise.all(
-        entries.map(async (entry) => {
-          const res = path.join(dir, entry.name)
-          const rel = path.join(base, entry.name)
-          if (entry.isDirectory()) return getAllMd(res, rel)
-          if (entry.isFile() && entry.name.endsWith('.md')) return [rel.replace(/\.md$/, '')]
-          return []
-        })
-      )
-      return files.flat()
-    }
-
-    const files = await getAllMd(path.join(process.cwd(), 'content', contentDir))
+    const files = await getAllPages(contentDir)
     return files.map((file) => ({
       [segmentKey]: file.split(path.sep),
     }))
