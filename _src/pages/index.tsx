3c35908626d4ad4b1c447c3ab53d0a2aed11e15c
Jayan Ratna <jayan@blutui.com>
2025-08-21 13:49:23 +1200

    fix index page

--- _src/pages/index.tsx
+++ _src/pages/index.tsx
@@ -4,7 +4,7 @@ import { SearchButton } from '@/components/search'
 import { FullPageLayout } from '@/layouts/full-page'
 
 import { Button } from '@/components/button'
-import { Card } from '@/components/card'
+import { Card } from 'components/card'
 
 import { AcademicCapOutline } from '@/components/icons/academic-cap'
 import { AtSymbol } from '@/components/icons/at-symbol'
@@ -62,11 +62,7 @@ const Home: NextPageWithLayout = () => {
       </div>
       <div className="max-w-8xl mx-auto mt-0 grid gap-6 px-8 lg:grid-cols-6">
         <div className="md:col-span-3">
-          <Card
-            title="Read the docs"
-            href="/docs/getting-started/create"
-            icon={<BookOpenOutline />}
-          >
+          <Card title="Read the docs" href="/docs/getting-started/create" icon={<BookOpenOutline />}>
             Learn how to get set up and start building your first project.
           </Card>
         </div>
@@ -76,38 +72,22 @@ const Home: NextPageWithLayout = () => {
           </Card>
         </div>
         <div className="md:col-span-3">
-          <Card
-            title="What's new in Blutui"
-            href="/changelog"
-            icon={<QueueListOutline />}
-          >
+          <Card title="What's new in Blutui" href="/changelog" icon={<QueueListOutline />}>
             {`See what's been added, changed, fixed or removed.`}
           </Card>
         </div>
         <div className="md:col-span-3">
-          <Card
-            title="YouTube tutorials"
-            href="https://www.youtube.com/@useBlutui"
-            icon={<Video />}
-          >
+          <Card title="YouTube tutorials" href="https://www.youtube.com/@useBlutui" icon={<Video />}>
             Watch developer tutorials on our YouTube channel.
           </Card>
         </div>
         <div className="md:col-span-3">
-          <Card
-            title="Discord"
-            href="https://discord.gg/4H8dZW6Fva"
-            icon={<Microphone />}
-          >
+          <Card title="Discord" href="https://discord.gg/4H8dZW6Fva" icon={<Microphone />}>
             Join the community of Blutui developers on Discord.
           </Card>
         </div>
         <div className="md:col-span-3">
-          <Card
-            title="Blutui on X"
-            href="https://x.com/blutui"
-            icon={<AtSymbol />}
-          >
+          <Card title="Blutui on X" href="https://x.com/blutui" icon={<AtSymbol />}>
             Follow us for product updates, tips and more.
           </Card>
         </div>
