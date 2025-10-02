51ea5b201d357b0fd11593b1812b9ca45fd51083
Jayan Ratna <jayan@blutui.com>
2025-10-03 11:58:55 +1300

    upgrade to MDX

--- source.config.ts
+++ source.config.ts
@@ -1,20 +1,24 @@
+import fs from 'node:fs'
 import { defineDocs, defineConfig } from 'fumadocs-mdx/config'
+import { remarkSteps } from 'fumadocs-core/mdx-plugins'
 
 export const docs = defineDocs({
   dir: 'content/docs',
 })
 
+const canvas = JSON.parse(fs.readFileSync('./lib/canvas.grammar.json', 'utf-8'))
+
 export default defineConfig({
   mdxOptions: {
     rehypeCodeOptions: {
-      langs: ['html', 'twig', 'bash'],
-      langAlias: {
-        canvas: 'twig',
-      },
+      lazy: true,
+      experimentalJSEngine: true,
+      langs: ['html', canvas, 'bash'],
       themes: {
         light: 'light-plus',
         dark: 'dark-plus',
       },
     },
+    recmaPlugins: [remarkSteps],
   },
 })
