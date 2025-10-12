945b246d4b7e764b9fcd7bcf30e6004c8076e740
Jayan Ratna <jayan@blutui.com>
2025-10-13 11:25:06 +1300

    add meta for api

--- components/docs-api.tsx
+++ components/docs-api.tsx
@@ -5,7 +5,7 @@ interface DocsApiProps {
 
 export function DocsApi({ method, endpoint }: DocsApiProps) {
   return (
-    <div className="bg-fd-primary/10 flex w-fit items-center gap-x-3 rounded-lg px-3 py-1.5">
+    <div className="bg-fd-primary/10 flex w-fit items-center gap-x-3 rounded-lg px-3 py-1.5 font-medium">
       <span className="font-mono text-sm">{method}</span>
       <span className="bg-fd-primary/50 size-1" />
       <span className="text-fd-primary font-mono text-sm">{endpoint}</span>
