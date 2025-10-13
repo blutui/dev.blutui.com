f46427fb68c8f91b8fc1d7d804ae9ac8df7b4351
Jayan Ratna <jayan@blutui.com>
2025-10-13 13:26:18 +1300

    exclude paths from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,6 +3,8 @@ import { MetadataRoute } from 'next'
 import { baseUrl } from 'lib/metadata'
 import { source } from 'lib/source'
 
+const exclude = ['-response-parameters', '-response', '-response-list', '/installation/create-courier-token']
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const url = (path: string): string => new URL(path, baseUrl).toString()
 
@@ -12,15 +14,20 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'monthly',
       priority: 1,
     },
-    ...source.getPages().map((page) => {
-      const { lastModified } = page.data
+    ...source
+      .getPages()
+      .filter((page) => {
+        return exclude.every((s) => !page.url.includes(s))
+      })
+      .map((page) => {
+        const { lastModified } = page.data
 
-      return {
-        url: url(page.url),
-        lastModified: lastModified ? new Date(lastModified) : undefined,
-        changeFrequency: 'weekly',
-        priority: 0.5,
-      } as MetadataRoute.Sitemap[number]
-    }),
+        return {
+          url: url(page.url),
+          lastModified: lastModified ? new Date(lastModified) : undefined,
+          changeFrequency: 'weekly',
+          priority: 0.5,
+        } as MetadataRoute.Sitemap[number]
+      }),
   ]
 }
