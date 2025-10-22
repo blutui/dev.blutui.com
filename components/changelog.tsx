a65b8528cbce8593ef377c163517e606068953bb
Jayan Ratna <jayan@blutui.com>
2025-10-22 14:18:32 +1300

    chore: Update changelog

--- components/changelog.tsx
+++ components/changelog.tsx
@@ -1,32 +1,46 @@
 interface ChangelogProps {
   date: string
+  type?: string
+  app?: string
   children: React.ReactNode
 }
 
-export function Changelog({ date, children }: ChangelogProps) {
+export function Changelog({ date, type, app, children }: ChangelogProps) {
   const id = date
   const formattedDate = new Date(date).toLocaleDateString('en-US', {
     year: 'numeric',
-    month: 'long',
+    month: 'short',
     day: 'numeric',
   })
 
   return (
     <section
       id={id}
-      className="flex w-full scroll-m-32 flex-col items-start gap-2 py-8 lg:flex-row lg:gap-6"
+      className="flex w-full scroll-m-32 flex-col items-start gap-2 border-b border-dashed py-4"
       aria-labelledby={`${id}-heading`}
     >
-      <div className="not-prose top-32 flex w-full shrink-0 flex-col items-start justify-start lg:sticky lg:w-40">
-        <div
-          id={`${id}-heading`}
-          className="bg-fd-primary/10 text-fd-primary flex flex-grow-0 items-center justify-center rounded-lg px-2 py-1 text-sm font-medium"
-        >
-          <a href={`#${id}`}>{formattedDate}</a>
-        </div>
+      <div className="not-prose flex w-full shrink-0 flex-col items-start justify-start">
+        <h3 id={`${id}-heading`} className="flex grow-0 flex-wrap items-center gap-2.5">
+          <time
+            dateTime={date}
+            className="inline-flex font-mono text-xs font-semibold tracking-tight text-neutral-500 uppercase"
+          >
+            {formattedDate}
+          </time>
+          {type && (
+            <span className="inline-flex rounded-sm bg-neutral-200 px-1.5 py-0.5 font-mono text-sm font-semibold tracking-tight uppercase dark:bg-neutral-800">
+              {type}
+            </span>
+          )}
+        </h3>
       </div>
-      <div className="max-w-full flex-1 px-0.5">
-        <div>{children}</div>
+      <div className="prose-p:my-3 flex w-full max-w-full flex-1 flex-col px-0.5 lg:flex-row lg:gap-4">
+        <div className="flex-1">{children}</div>
+        {app && (
+          <div className="shrink-0 lg:mt-3">
+            <span className="font-mono text-xs font-semibold text-neutral-500 uppercase">{app}</span>
+          </div>
+        )}
       </div>
     </section>
   )
