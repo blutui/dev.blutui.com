d581d8e64fa327d69993cdd74f541b44ec4ffc88
Jayan Ratna <jayan@blutui.com>
2025-10-28 14:10:58 +1300

    fix: favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,13 @@ export const metadata = createMetadata({
   },
   description: 'Welcome to the Blutui Developer documentation',
   metadataBase: baseUrl,
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 })
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
