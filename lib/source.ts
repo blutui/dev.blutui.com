dd9a7652b214640b6e9f3bf9c1dfbf8631f84a5a
Jayan Ratna <jayan@blutui.com>
2025-12-22 10:51:21 +1300

    fix

--- lib/source.ts
+++ lib/source.ts
@@ -1,16 +1,12 @@
-import { type InferMetaType, type InferPageType, loader, multiple } from 'fumadocs-core/source'
+import { type InferMetaType, type InferPageType, loader } from 'fumadocs-core/source'
 import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons'
 import { docs } from 'fumadocs-mdx:collections/server'
 
-export const source = loader(
-  multiple({
-    docs: docs.toFumadocsSource(),
-  }),
-  {
-    baseUrl: '/',
-    plugins: [lucideIconsPlugin()],
-  }
-)
+export const source = loader({
+  baseUrl: '/',
+  source: docs.toFumadocsSource(),
+  plugins: [lucideIconsPlugin()],
+})
 
 export type Page = InferPageType<typeof source>
 export type Meta = InferMetaType<typeof source>
