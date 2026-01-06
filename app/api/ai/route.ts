7cef3c7e4cfb4266df7b11f2430deb507a400681
dion-blutui <dion@blutui.com>
2026-01-07 10:55:35 +1300

    use /ai

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -1,7 +1,7 @@
 export async function POST(request: Request) {
   const { query } = await request.json()
 
-  const res = await fetch(process.env.AI_SEARCH_API ?? 'https://ai-search-api.blutui.workers.dev', {
+  const res = await fetch(process.env.AI_SEARCH_API ?? 'https://ai-search-api.blutui.workers.dev/ai', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
